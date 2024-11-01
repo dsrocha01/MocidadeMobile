27b31c58027a6c32c443a534ad3a5b4d7aa07443
Douglas Rocha <dsrocha01@gmail.com>
2024-11-01 01:37:51 -0300

    Primeiro Commit do app

---
+++ MocidadeMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MocidadeMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MocidadeMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MocidadeMobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
